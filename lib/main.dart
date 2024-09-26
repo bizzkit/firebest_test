94faa42feabe7ee4c6b45eb3989dbd72b4eafad3
bizkit <konovalov-neo911@mail.ru>
2024-09-26 17:25:12 +0300

    last fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:notify_inapp/notify_inapp.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
   print("Handling a background message: ${message.messageId}");
 }
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
@@ -41,26 +47,169 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class NotificationService {
+  Future<void> setFCMToken(String token) async {
+    // Здесь вы можете отправить токен на ваш сервер или использовать его по вашему усмотрению.
+    print("FCM Token set: $token");
+    // Пример: await ApiService.sendTokenToServer(token);
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   late FirebaseMessaging messaging;
   String? _token;
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  Notify notify = Notify();
 
   @override
   void initState() {
     super.initState();
-    messaging = FirebaseMessaging.instance;
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    _createNotificationChannel();
+    initFirebase();
+  }
 
-    messaging.getToken().then((token) {
-      setState(() {
-        _token = token;
+  initFirebase() async {
+    final notificationSettings =
+        await FirebaseMessaging.instance.requestPermission(
+      provisional: false,
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      sound: true,
+    );
+
+    if (notificationSettings.authorizationStatus ==
+        AuthorizationStatus.authorized) {
+      final fcmToken = await FirebaseMessaging.instance.getToken();
+
+      if (fcmToken != null) {
+        await NotificationService().setFCMToken(fcmToken);
+        setState(() {
+          _token = fcmToken; // Сохраняем токен в переменной и обновляем интерфейс
+        });
+      }
+
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        _handleMessage(message);
       });
-      print("FCM Token: $token");
-    });
+    }
+  }
 
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-    });
+  Future<void> _handleMessage(RemoteMessage message) async {
+    // foreground notification
+    if (message.notification != null) {
+      notify.show(
+        context,
+        view(message.notification?.title ?? '-', message.notification?.body ?? '-'),
+      );
+    }
+  }
+
+  Widget view(String title, String text) {
+    return GestureDetector(
+      onTap: () {
+        if (notify.isShown()) {
+          notify.dismiss(false);
+        }
+      },
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Container(
+          padding: const EdgeInsets.only(
+            left: 25,
+            right: 25,
+            top: 15,
+            bottom: 15,
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: const [
+              BoxShadow(
+                spreadRadius: 0,
+                color: Color(0x4d000000),
+                blurRadius: 15,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Color(0xff333333),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      text,
+                      style: const TextStyle(
+                        color: Color(0xff333333),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _createNotificationChannel() async {
+    if (Platform.isAndroid) {
+      const AndroidNotificationChannel channel = AndroidNotificationChannel(
+        'default_channel_id',
+        'Default notifications',
+        description: 'Channel for default notifications',
+        importance: Importance.max,
+      );
+
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>()
+          ?.createNotificationChannel(channel);
+
+      print('Notification channel created');
+    }
+  }
+
+  Future<void> _showNotification(RemoteNotification notification) async {
+    print('Notification Title: ${notification.title}');
+    print('Notification Body: ${notification.body}');
+
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'default_channel_id',
+      'Default notifications',
+      channelDescription: 'Channel for default notifications',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      platformChannelSpecifics,
+    );
   }
 
   @override
